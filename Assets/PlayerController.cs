e1dcbe9382482835a7fc8a9e252f41e03bb16f0b
Kutterhead <esseanx@gmail.com>
2025-12-11 22:36:19 +0100

    Implementado movimiento con eje horizontal.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,68 +4,49 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-
+    Rigidbody rigidbody;
     public PlayerInput playerInput;
-
     public InputAction moveH;
-
-    public InputAction fordward;
-    public InputAction backward;
     public InputAction jump;
-
-
+    public InputAction down;
     public Animator animator;
-
-
-
-
-    
+  
     void Start()
     {
-        moveH = playerInput.actions.FindAction("Forward");
-        fordward = playerInput.actions.FindAction("Forward");
-        backward = playerInput.actions.FindAction("Backward");
+        rigidbody = GetComponent<Rigidbody>();
+        moveH = playerInput.actions.FindAction("MoveH");
+        down = playerInput.actions.FindAction("Down");
         jump = playerInput.actions.FindAction("Jump");
     }
 
     // Update is called once per frame
+    float movimientoH = 0f;
+    public float velocidadX = 1f;
     void Update()
     {
-        Debug.Log("Movimiento: " + ((moveH.ReadValue<float>() * 2) - 1f) );
-
-
-        
-        if (fordward.triggered)//getkeyDown
-        {
-            //Debug.Log("tecla pulsada");
-
-        }
-        if (fordward.IsPressed())//getkey
-        {
-            //Debug.Log("tecla pulsada");
-            animator.SetBool("Forward", true);
-
-
-        }
-        else
-        {
-            animator.SetBool("Forward", false);
-        }
-
-
-
-        if (backward.IsPressed())//getkey
-        {
-           // Debug.Log("tecla pulsada");
-            animator.SetBool("Backward",true);
-
-
-        }
-        else
-        {
-            animator.SetBool("Backward", false);
-        }
 
 
     }
+    public float salto = 10f;
+    private void FixedUpdate()
+    {
+        movimientoH = moveH.ReadValue<float>();
+
+        Debug.Log("Movimiento: " + movimientoH);
+
+        // rigidbody.linearVelocity = transform.forward * movimientoH * velocidadX;
+        if (jump.triggered)
+        {
+        rigidbody.linearVelocity = new Vector3(rigidbody.linearVelocity.x, rigidbody.linearVelocity.y + salto, rigidbody.linearVelocity.z);
+            //rigidbody.linearVelocity = new Vector3(rigidbody.linearVelocity.x, salto, rigidbody.linearVelocity.z); 
+         //   rigidbody.linearVelocity += transform.up * salto;
+
+        }
+
+
+
+
+    }
+
+
 }
